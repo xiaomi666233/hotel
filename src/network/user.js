1dc45016db1d6b50785048af77481c6632375721
liangzhu <1678881243@qq.com>
2020-04-18 22:03:12 +0800

    完善

--- src/network/user.js
+++ src/network/user.js
@@ -1,6 +1,6 @@
 import { request } from "./request";
 // 订房
-export function order(data) {
+export function order (data) {
     return request({
         url: '/room/order',
         method: "post",
@@ -8,49 +8,49 @@ export function order(data) {
     })
 }
 // 查询空房
-export function room() {
+export function room () {
     return request({
         url: '/room/room',
         method: "get",
     })
 }
 // 查询订房信息
-export function roomOrder(pageNum, pageSize) {
+export function roomOrder (pageNum, pageSize) {
     return request({
         url: `/roomOrder/roomOrder?pageNum=${pageNum}&pageSize=${pageSize}`,
         method: "get",
     })
 }
 // 确定入住
-export function roomOrderC(id) {
+export function roomOrderC (id) {
     return request({
         url: `/roomOrder/roomOrder/${id}`,
         method: "put",
     })
 }
 // 取消入住
-export function roomOrderD(id) {
+export function roomOrderD (id) {
     return request({
         url: `/roomOrder/roomOrder/${id}`,
         method: "delete",
     })
 }
 // 查找全部房间
-export function roomGet() {
+export function roomGet () {
     return request({
         url: `/room/room`,
         method: "get",
     })
 }
 // 查找指定房间
-export function roomGetOne(id) {
+export function roomGetOne (id) {
     return request({
         url: `/room/room/${id}`,
         method: "put",
     })
 }
 // 修改房间价格
-export function roomMP(data) {
+export function roomMP (data) {
     return request({
         url: `/room/modifyRoomPrice`,
         method: "post",
@@ -58,7 +58,7 @@ export function roomMP(data) {
     })
 }
 // 续住
-export function roomR(data) {
+export function roomR (data) {
     return request({
         url: `/room/roomRenewal`,
         method: "post",
@@ -67,9 +67,26 @@ export function roomR(data) {
 }
 
 // 超时
-export function roomCheck() {
+export function roomCheck (pageNum, pageSize) {
     return request({
-        url: `/roomOrder/overtimeRoomOrder`,
+        url: `/roomOrder/overtimeRoomOrder?pageNum=${pageNum}&pageSize=${pageSize}`,
+        method: "get"
+    })
+}
+
+// 根据预约时间查空房
+export function timeGetkf (data) {
+    return request({
+        url: `/room/spareRoom?yysj=${data}`,
+        method: "get"
+    })
+}
+
+
+// 根据房号查询
+export function fhGetRoom (fh, pageNum, pageSize) {
+    return request({
+        url: `/roomOrder/roomOrderByFh?fh=${fh}&pageNum=${pageNum}&pageSize=${pageSize}`,
         method: "get"
     })
 }
--- src/pages/orderMsg.vue
+++ src/pages/orderMsg.vue
@@ -1,28 +1,42 @@
 <template>
   <div>
     <div class="header">
-      <Input placeholder="输入房号" style="width: 300px" />
-      <Button type="success">搜索</Button>
+      <Input placeholder="输入房号"
+             style="width: 300px"
+             v-model="fh" />
+      <Button type="success"
+              @click="getRoomByfh()">搜索</Button>
     </div>
     <div>
-      <Table border :columns="columns1" :data="orderList">
-        <template slot-scope="{ row, index }" slot="action">
-          <Button type="warning" size="small" @click="modifyData(index)">修改</Button>
-          <Button
-            type="primary"
-            :disabled="row.yrz == '是'"
-            size="small"
-            @click="confirm(row.roomOrderUuid)"
-          >确认入住</Button>
-          <Button type="error" size="small" @click="okqxrz(row.roomOrderUuid)">取消入住</Button>
+      <Table border
+             :columns="columns1"
+             :data="orderList">
+        <template slot-scope="{ row, index }"
+                  slot="action">
+          <Button type="warning"
+                  size="small"
+                  @click="modifyData(index)">修改</Button>
+          <Button type="primary"
+                  :disabled="row.yrz == '是'"
+                  size="small"
+                  @click="confirm(row.roomOrderUuid)">确认入住</Button>
+          <Button type="error"
+                  size="small"
+                  @click="okqxrz(row.roomOrderUuid)">取消入住</Button>
         </template>
       </Table>
     </div>
     <div class="page">
-      <Page :total="100" @on-change="selectMsg" />
+      <Page :total="total"
+            :page-size="10"
+            @on-change="selectMsg" />
     </div>
-    <Modal title="修改" v-model="modify" class-name="vertical-center-modal" @on-ok="okModify()">
-      <Form label-position="left" :label-width="100">
+    <Modal title="修改"
+           v-model="modify"
+           class-name="vertical-center-modal"
+           @on-ok="okModify()">
+      <Form label-position="left"
+            :label-width="100">
         <FormItem label="订单类型">
           <Select v-model="msg.ddlx">
             <Option value="预订单">预订单</Option>
@@ -33,24 +47,20 @@
           <Input v-model="msg.sjhm"></Input>
         </FormItem>
         <FormItem label="预约时间">
-          <DatePicker
-            type="date"
-            @on-change="handleChangeyy"
-            format="yyyy-MM-dd"
-            v-model="msg.yysj"
-            placeholder="选择预约时间"
-            style="width: 100%"
-          ></DatePicker>
+          <DatePicker type="date"
+                      @on-change="handleChangeyy"
+                      format="yyyy-MM-dd"
+                      v-model="msg.yysj"
+                      placeholder="选择预约时间"
+                      style="width: 100%"></DatePicker>
         </FormItem>
         <FormItem label="预离时间">
-          <DatePicker
-            type="date"
-            @on-change="handleChangeyl"
-            format="yyyy-MM-dd"
-            v-model="msg.ylsj"
-            placeholder="选择预约时间"
-            style="width: 100%"
-          ></DatePicker>
+          <DatePicker type="date"
+                      @on-change="handleChangeyl"
+                      format="yyyy-MM-dd"
+                      v-model="msg.ylsj"
+                      placeholder="选择预约时间"
+                      style="width: 100%"></DatePicker>
         </FormItem>
         <FormItem label="总价格">
           <Input v-model="msg.fjzjg"></Input>
@@ -73,128 +83,161 @@
 </template>
 
 <script>
-import { roomOrder, roomOrderC, roomOrderD } from "@/network/user.js";
+import { roomOrder, roomOrderC, roomOrderD, fhGetRoom } from '@/network/user.js'
 export default {
   data() {
     return {
+      fh: '',
+      total: 0,
+      page: 1,
       modify: false,
       confirmLive: false,
       modifyLive: false,
       select: 0,
       msg: {
-        fh: "",
-        ddh: "",
-        ddlx: "",
-        sjhm: "",
-        yysj: "",
-        ylsj: "",
+        fh: '',
+        ddh: '',
+        ddlx: '',
+        sjhm: '',
+        yysj: '',
+        ylsj: '',
         fjzjg: 0,
-        yrz: ""
+        yrz: ''
       },
       columns1: [
         {
-          title: "房号",
-          key: "fh",
+          title: '房号',
+          key: 'fh',
           width: 70
         },
         {
-          title: "订单号",
-          key: "ddh",
+          title: '订单号',
+          key: 'ddh',
           width: 210
         },
         {
-          title: "订单类型",
-          key: "ddlx"
+          title: '订单类型',
+          key: 'ddlx'
         },
         {
-          title: "手机号码",
-          key: "sjhm"
+          title: '手机号码',
+          key: 'sjhm'
         },
         {
-          title: "预约时间",
-          key: "yysj"
+          title: '预约时间',
+          key: 'yysj'
         },
         {
-          title: "预离时间",
-          key: "ylsj"
+          title: '预离时间',
+          key: 'ylsj'
         },
         {
-          title: "订单总价格",
-          key: "fjzjg"
+          title: '订单总价格',
+          key: 'fjzjg'
         },
         {
-          title: "已入住",
-          key: "yrz"
+          title: '已入住',
+          key: 'yrz'
         },
         {
-          title: "操作",
-          slot: "action",
+          title: '操作',
+          slot: 'action',
           width: 250,
-          align: "center"
+          align: 'center'
         }
       ],
       orderList: []
-    };
+    }
   },
   created() {
-    this.getRoomOrderList();
+    this.getRoomOrderList()
   },
   computed: {},
+  watch: {
+    fh: function(o, n) {
+      if (!!n) {
+        this.getRoomOrderList()
+      }
+    }
+  },
   methods: {
     minToymd(min) {
-      let date = new Date(min);
-      let y = date.getFullYear();
-      let m = date.getMonth();
-      let d = date.getDate();
-      return y + "-" + m + "-" + d;
+      let date = new Date(min)
+      let y = date.getFullYear()
+      let m = date.getMonth()
+      let d = date.getDate()
+      return y + '-' + m + '-' + d
     },
     getRoomOrderList() {
-      let that = this;
-      roomOrder(1, 10).then(res => {
+      roomOrder(this.page, 10).then(res => {
         if (res.status === 200) {
-          that.orderList = res.data.response;
-          for (let i = 0; i < that.orderList.length; i++) {
-            that.orderList[i].yysj = that.minToymd(that.orderList[i].yysj.time);
-            that.orderList[i].ylsj = that.minToymd(that.orderList[i].ylsj.time);
+          this.total = res.data.total
+          this.orderList = res.data.response
+          for (let i = 0; i < this.orderList.length; i++) {
+            this.orderList[i].yysj = this.minToymd(this.orderList[i].yysj.time)
+            this.orderList[i].ylsj = this.minToymd(this.orderList[i].ylsj.time)
           }
         }
-      });
+      })
     },
     modifyData(index) {
-      this.modify = true;
-      Object.assign(this.msg, this.orderList[index]);
+      this.modify = true
+      Object.assign(this.msg, this.orderList[index])
     },
     handleChangeyy(date) {
-      this.msg.btime = date;
+      this.msg.btime = date
     },
     handleChangeyl(date) {
-      this.msg.ylsj = date;
+      this.msg.ylsj = date
     },
     okModify() {
       // 发请求
-      this.$Message.success("修改成功");
+      this.$Message.success('修改成功')
     },
     okqxrz(id) {
       roomOrderD(id).then(res => {
         if (res.status === 200) {
-          this.getRoomOrderList();
-          this.$Message.success("取消成功");
+          this.getRoomOrderList()
+          this.$Message.success('取消成功')
         }
-      });
+      })
     },
     selectMsg(index) {
-      this.$Message.success("切换到 " + index + " 页");
+      if (this.fh !== '') {
+        this.page = index
+        this.getRoomByfh()
+        this.$Message.success('切换到 ' + index + ' 页')
+      } else {
+        this.page = index
+        this.getRoomOrderList()
+        this.$Message.success('切换到 ' + index + ' 页')
+      }
     },
     confirm(id) {
       roomOrderC(id).then(res => {
         if (res.status === 200) {
-          this.getRoomOrderList();
-          this.$Message.success("确认成功");
+          this.getRoomOrderList()
+          this.$Message.success('确认成功')
         }
-      });
+      })
+    },
+    getRoomByfh() {
+      if (this.fh == '') return this.$Message.error('请输入房号')
+      fhGetRoom(this.fh, this.page, 10).then(res => {
+        if (res.status === 200) {
+          if (res.data.total === 0)
+            this.$Message.error('没有找到该房号的订单信息')
+          this.total = res.data.total
+          this.orderList = res.data.response
+          for (let i = 0; i < this.orderList.length; i++) {
+            this.orderList[i].yysj = this.minToymd(this.orderList[i].yysj.time)
+            this.orderList[i].ylsj = this.minToymd(this.orderList[i].ylsj.time)
+          }
+        }
+      })
     }
   }
-};
+}
 </script>
 
 <style scoped>
